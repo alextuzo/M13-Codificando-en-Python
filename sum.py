84f07b684c70c0acbccf128a6a803c902d1385f1
aleja <alejandroarrieta2010@hotmail.com>
2024-08-06 13:37:11 -0600

    initial commit

---
+++ sum.py
@@ -0,0 +1,18 @@
+
+sum  # ********************
+
+
+# LA SUMA MÁS SENCILLA
+# ********************
+
+
+def run(a: int, b: int) -> int:
+    # TU CÓDIGO AQUÍ
+
+    result = a + b
+
+    return result
+
+
+if __name__ == '__main__':
+    run(3, 4)
